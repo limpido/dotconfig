a56a5a15f8252e7273bde987dede2ee517025d14
limpido <43034953+limpido@users.noreply.github.com>
2025-03-30 21:39:30 +0800

    update fish function: l (alias eza)

--- .config/fish/functions/l.fish
+++ .config/fish/functions/l.fish
@@ -1,4 +1,3 @@
 function l --wraps='eza --long -g -o --all' --wraps='eza --long -g -o' --description 'alias l eza --long -g -o'
-  eza --long -g -o $argv
-        
+  eza --long -g -o --total-size $argv
 end
